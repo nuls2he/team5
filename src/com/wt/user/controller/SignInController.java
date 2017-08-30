57706e2c15b956e1378cced2658c18598e76d476
Sang Ho <ASUS@DESKTOP-0B5UUE1>
2017-08-30 19:28:05 +0900

    aa

--- src/com/wt/user/controller/SignInController.java
+++ src/com/wt/user/controller/SignInController.java
@@ -1,9 +1,20 @@
 package com.wt.user.controller;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/controller/signin")
 public class SignInController extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		
+	}
 	
 }
