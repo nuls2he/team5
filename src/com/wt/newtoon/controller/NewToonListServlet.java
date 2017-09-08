54455e09d07c513427ec1d78340283bf1b6669e2
Bitcamp\Bit <jaeik@hanmail.net>
2017-09-08 12:31:05 +0900

    no message

--- src/com/wt/newtoon/controller/NewToonListServlet.java
+++ src/com/wt/newtoon/controller/NewToonListServlet.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.wt.common.dao.CommonDao;
 import com.wt.common.domain.Common;
@@ -18,7 +18,7 @@ import com.wt.common.domain.Search;
 public class NewToonListServlet extends HttpServlet{
 	
 	@Override
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		CommonDao dao = new CommonDao();
 		int rnum = 0;
--- src/com/wt/newtoon/controller/NewToondetailServlet.java
+++ src/com/wt/newtoon/controller/NewToondetailServlet.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.wt.common.dao.CommonDao;
 import com.wt.common.domain.Common;
@@ -19,7 +19,7 @@ public class NewToondetailServlet extends HttpServlet{
 	CommonDao dao = new CommonDao();
 	
 	@Override
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		Search vo = new Search();
 		request.setCharacterEncoding("utf-8");
